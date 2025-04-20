e6d09cfb3832eb9a66e3aff792344824f9e5874f
Umar Adelowo <umaradelo1.247@gmail.com>
2025-04-20 12:48:28 +0100

    Corrected MaxArrayDeque  implementation

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -3,50 +3,23 @@ package deque;
 import java.util.Comparator;
 
 public class MaxArrayDeque<T> extends ArrayDeque<T> {
-    T max;
-    Comparator<T> c;
+    private Comparator<T> c;
 
     public MaxArrayDeque(Comparator<T> c) {
         this.c = c;
     }
 
     public T max() {
-        return max;
+        return max(c);
     }
 
-    public T max(Comparator<T> c) {
-        T largest  = get(0);
+    public T max(Comparator<T> comparer) {
+        T max  = get(0);
         for (var x: this) {
-            if (c.compare(largest, x) < 0) {
-                largest = x;
+            if (comparer.compare(max, x) < 0) {
+                max = x;
             }
         }
-        return largest;
-    }
-
-    @Override
-    public void addFirst(T item) {
-        super.addFirst(item);
-        max = max(c);
-    }
-
-    @Override
-    public void addLast(T item) {
-        super.addLast(item);
-        max = max(c);
-    }
-
-    @Override
-    public T removeFirst() {
-        T first = super.removeFirst();
-        max = max(c);
-        return first;
-    }
-
-    @Override
-    public T removeLast() {
-        T last = super.removeLast();
-        max = max(c);
-        return last;
+        return max;
     }
 }
